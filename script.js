cf970b32c5d003dc02e087a8e3ba57f8cdf2b5c4
Gargi <98906186+GargiMittal@users.noreply.github.com>
2024-01-08 14:24:25 +0530

    added download pdf button

--- script.js
+++ script.js
@@ -112,3 +112,15 @@ console.log("checkpoint");
 function printCV(){
     window.print();
 }
+
+function downloadPDF() {
+    const element = document.getElementById('cv-templete');
+
+    html2pdf(element, {
+        margin: 10,
+        filename: 'resume.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    });
+}
